b0bb63dcbc33a5806df5c0cdd6a81e82c0ee8cf1
MasoudMaghsodi <magsodi333@gmail.com>
2025-01-03 00:39:52 +0330

    last

--- lib/main.dart
+++ lib/main.dart
@@ -14,102 +14,83 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.purple.shade700,
         body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // دکمه خانه
-                      CustomGameButton(
-                        size: 80,
-                        icon: Icons.home,
-                        backgroundColor: Colors.orange.shade800,
-                        borderColor: Colors.grey.shade200,
-                        shadowColor: Colors.black54,
-                        borderRadius: 13,
-                        onTap: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "Home pressed",
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              duration: Duration(milliseconds: 500),
-                              backgroundColor: Colors.orange,
-                            ),
-                          );
-                        },
-                      ),
-                      const SizedBox(width: 16),
-                      // دکمه توقف
-                      CustomGameButton(
-                        size: 80,
-                        icon: Icons.pause,
-                        backgroundColor: Colors.purple.shade700,
-                        borderColor: Colors.grey.shade200,
-                        shadowColor: Colors.black54,
-                        borderRadius: 50,
-                        onTap: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "Pause pressed",
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              duration: Duration(milliseconds: 500),
-                              backgroundColor: Colors.purple,
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  // دکمه پخش
-                  CustomGameButton(
-                    size: 80,
-                    text: "PLAY",
-                    icon: Icons.play_arrow,
-                    backgroundColor: Colors.blueAccent.shade700,
-                    borderColor: Colors.grey.shade300,
-                    shadowColor: Colors.black54,
-                    borderRadius: 13,
-                    onTap: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                            "Play pressed",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          duration: Duration(milliseconds: 500),
-                          backgroundColor: Colors.blue,
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-            },
-          ),
+          child: Builder(builder: (context) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomGameButton(
+                      size: 80,
+                      icon: Icons.home,
+                      backgroundColor: Colors.orange.shade800,
+                      borderColor: Colors.grey.shade200,
+                      shadowColor: Colors.black54,
+                      borderRadius: 13,
+                      onTap: () {
+                        _showSnackbar(context, "Home pressed", Colors.orange);
+                      },
+                    ),
+                    const SizedBox(width: 16),
+                    CustomGameButton(
+                      size: 80,
+                      icon: Icons.pause,
+                      backgroundColor: Colors.purple.shade700,
+                      borderColor: Colors.grey.shade200,
+                      shadowColor: Colors.black54,
+                      borderRadius: 50,
+                      onTap: () {
+                        _showSnackbar(context, "Pause pressed", Colors.purple);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                CustomGameButton(
+                  size: 80,
+                  text: "PLAY",
+                  icon: Icons.play_arrow,
+                  backgroundColor: Colors.blueAccent.shade700,
+                  borderColor: Colors.grey.shade300,
+                  shadowColor: Colors.black54,
+                  borderRadius: 13,
+                  onTap: () {
+                    _showSnackbar(context, "Play pressed", Colors.blue);
+                  },
+                ),
+              ],
+            );
+          }),
         ),
+      ),
+    );
+  }
+
+  void _showSnackbar(
+      BuildContext context, String message, Color backgroundColor) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(color: Colors.black),
+        ),
+        duration: const Duration(milliseconds: 500),
+        backgroundColor: backgroundColor,
       ),
     );
   }
 }
 
-// ویجت قابل سفارشی‌سازی برای دکمه بازی
 class CustomGameButton extends StatefulWidget {
-  final String? text; // متن دکمه (در صورت وجود)
-  final IconData? icon; // آیکون دکمه
-  final Color backgroundColor; // رنگ پس‌زمینه
-  final Color borderColor; // رنگ مرز
-  final Color shadowColor; // رنگ سایه
-  final double borderRadius; // شعاع گوشه‌ها
-  final double size; // اندازه دکمه
-  final VoidCallback onTap; // تابع فراخوانی هنگام لمس
+  final String? text;
+  final IconData? icon;
+  final Color backgroundColor;
+  final Color borderColor;
+  final Color shadowColor;
+  final double borderRadius;
+  final double size;
+  final VoidCallback onTap;
 
   const CustomGameButton({
     super.key,
@@ -129,7 +110,7 @@ class CustomGameButton extends StatefulWidget {
 }
 
 class _CustomGameButtonState extends State<CustomGameButton> {
-  bool _isPressed = false; // وضعیت فشرده بودن دکمه
+  bool _isPressed = false;
 
   @override
   Widget build(BuildContext context) {
@@ -138,60 +119,94 @@ class _CustomGameButtonState extends State<CustomGameButton> {
       onTapDown: (_) => setState(() => _isPressed = true),
       onTapUp: (_) => setState(() => _isPressed = false),
       onTapCancel: () => setState(() => _isPressed = false),
+      child: Stack(
+        children: [
+          _buildOuterShadow(),
+          _buildButtonContent(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildOuterShadow() {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.easeInOut,
+      height: widget.size,
+      width: widget.text != null ? widget.size * 2.5 : widget.size,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade100,
+        borderRadius: BorderRadius.circular(widget.borderRadius),
+        boxShadow: _isPressed
+            ? []
+            : [
+                BoxShadow(
+                  color: widget.shadowColor.withValues(alpha: 0.6),
+                  offset: const Offset(2, 3),
+                  blurRadius: 5,
+                ),
+              ],
+      ),
+    );
+  }
+
+  Widget _buildButtonContent() {
+    return Positioned.fill(
       child: AnimatedContainer(
-        duration: const Duration(milliseconds: 100), // مدت زمان انیمیشن
-        curve: Curves.easeInOut, // منحنی انیمیشن
-        height: widget.size, // ارتفاع دکمه
-        width:
-            widget.text != null ? widget.size * 2.5 : widget.size, // عرض دکمه
+        duration: const Duration(milliseconds: 100),
+        margin: const EdgeInsets.all(4.5),
         decoration: BoxDecoration(
           color: widget.backgroundColor,
-          // افکت گرادیانت برای ایجاد حالت سه‌بعدی
-          gradient: LinearGradient(
-            colors: _isPressed
-                ? [
-                    widget.backgroundColor,
-                    widget.backgroundColor,
-                  ]
-                : [
-                    Colors.white.withValues(alpha: 0.3), // نوار روشن در بالا
-                    widget.backgroundColor,
-                  ],
-            stops: [0.0, 0.35], // نسبت تقسیم گرادیانت
-            begin: Alignment.topCenter, // شروع از بالا
-            end: Alignment.center, // پایان در وسط
-          ),
-          border: Border.all(color: widget.borderColor, width: 6), // مرز دکمه
-          borderRadius:
-              BorderRadius.circular(widget.borderRadius), // گردی گوشه‌ها
+          borderRadius: BorderRadius.circular(widget.borderRadius),
           boxShadow: _isPressed
               ? []
               : [
-                  // سایه برای حالت غیر فشرده
                   BoxShadow(
                     color: widget.shadowColor.withValues(alpha: 0.6),
-                    offset: const Offset(0, 6),
-                    blurRadius: 12,
+                    offset: const Offset(1, 2),
+                    blurRadius: 5,
                   ),
                 ],
+          border: _isPressed
+              ? null
+              : Border(
+                  top: BorderSide(
+                    color: Colors.black38,
+                    width: 1.5,
+                    style: BorderStyle.solid,
+                  ),
+                  left: BorderSide(
+                    color: Colors.black38,
+                    width: 1.5,
+                    style: BorderStyle.solid,
+                  ),
+                  right: BorderSide(
+                    color: Colors.black38,
+                    width: 1.5,
+                    style: BorderStyle.solid,
+                  ),
+                  bottom: BorderSide(
+                    color: Colors.black38,
+                    width: 1.5,
+                    style: BorderStyle.solid,
+                  ),
+                ),
         ),
         child: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: [
-              // نمایش متن دکمه در صورت وجود
               if (widget.text != null)
                 Text(
                   widget.text!,
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontSize: widget.size / 4, // اندازه متن
+                    fontSize: widget.size / 4,
                     shadows: _isPressed
                         ? []
                         : [
-                            // سایه متن برای حالت سه‌بعدی
                             BoxShadow(
                               color: widget.shadowColor.withValues(alpha: 0.6),
                               offset: const Offset(2, 2),
@@ -201,7 +216,7 @@ class _CustomGameButtonState extends State<CustomGameButton> {
                   ),
                 ),
               if (widget.text != null && widget.icon != null)
-                const SizedBox(width: 8), // فاصله بین متن و آیکون
+                const SizedBox(width: 8),
               if (widget.icon != null)
                 Icon(
                   widget.icon,
@@ -210,7 +225,6 @@ class _CustomGameButtonState extends State<CustomGameButton> {
                   shadows: _isPressed
                       ? []
                       : [
-                          // سایه آیکون برای حالت سه‌بعدی
                           BoxShadow(
                             color: widget.shadowColor.withValues(alpha: 0.6),
                             offset: const Offset(2, 2),
